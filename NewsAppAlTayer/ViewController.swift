85616d02a36310c7ec0eeb7828961a03b167514d
Fazeel Ahmed <fazeel.a@sunandsandsports.com>
2020-07-09 22:47:11 +0400

    Initial Commit

---
+++ NewsAppAlTayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppAlTayer
+//
+//  Created by Fazeel Ahmed on 7/9/20.
+//  Copyright © 2020 Sun and Sand Sports LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
